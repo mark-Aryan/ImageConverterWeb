e1e488c56b73ce0bf7ee8d1be406fd6b03bdc4be
mark-Aryan <akj4073@gmail.com>
2023-08-15 16:24:18 +0530

    Updated error

--- Web_Image.py
+++ Web_Image.py
@@ -1,14 +1,18 @@
 import streamlit as st
 from PIL import Image
 
-# Starting camera by creating instance of camera input
-camera_image = st.camera_input("Camera")
+st.info("Creator: Aryan Kumar Upadhyay")
 
-# Creating instance of the Image
-img = Image.open(camera_image)
+with st.expander("Start Camera"):
+    # Starting camera by creating instance of camera input
+    camera_image = st.camera_input("Camera")
 
-# Converting image into grayscale
-gray_img = img.convert("L")
+if camera_image:
+    # Creating instance of the Image
+    img = Image.open(camera_image)
 
-# Rendering the image
-st.image(gray_img)
+    # Converting image into grayscale
+    gray_img = img.convert("L")
+
+    # Rendering the image
+    st.image(gray_img)

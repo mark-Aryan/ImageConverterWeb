44f22309f0d11ffaf982640f1a171a6593d7074f
mark-Aryan <akj4073@gmail.com>
2023-08-15 16:46:39 +0530

    Upload file feature added

--- Web_Image.py
+++ Web_Image.py
@@ -1,11 +1,21 @@
 import streamlit as st
 from PIL import Image
 
+
+st.title("Image to Grayscale Converter.")
+
 st.info("Creator: Aryan Kumar Upadhyay")
 
-with st.expander("Start Camera"):
+uploaded_image = st.file_uploader("Upload Image")
+
+with st.expander("Click image on the spot!"):
     # Starting camera by creating instance of camera input
     camera_image = st.camera_input("Camera")
+
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    gray_img = img.convert("L")
+    st.image(gray_img)
 
 if camera_image:
     # Creating instance of the Image
